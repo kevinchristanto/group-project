7a6bba53937f23c89b50a3402a780b5c580912c8
Ryan Martin <ryan.mrt1n@gmail.com>
2020-09-22 21:42:03 +0700

    added new db function to query record

--- db.c
+++ db.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sqlite3.h>
 #include "db.h"
 
+// open database and do error checking
+static int open_database(sqlite3 *db) {
+    int rc = sqlite3_open("database.db", &db);
+    if (rc) {
+        fprintf(stderr, "error opening database: %s\n", sqlite3_errmsg(db));
+        exit(1);
+    }
+    return rc;
+}
+
+// used in sqlite3_exec func
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
    for(int i = 0; i < argc; i++) {
      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
@@ -12,11 +24,10 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
    return 0;
 }
 
-
+// insert row into products table of database
 void db_insert_products(
     sqlite3 *db, char *name, char *category, int price, int stock) {
-
-    int rc = sqlite3_open("database.db", &db);
+    int rc = open_database(db);
 
     char sql[128];
     char *err_msg;
@@ -26,6 +37,7 @@ void db_insert_products(
         values ('%s', '%s', %d, %d);",
         name, category, price, stock);
 
+    // execute sql command to db
     rc = sqlite3_exec(db, sql, callback, 0, &err_msg);
 
     if (rc != SQLITE_OK) {
@@ -35,13 +47,15 @@ void db_insert_products(
         fprintf(stdout, "Records created successfully\n");
     }
 
+    // close connection
     sqlite3_close(db);
 }
 
+// delete record in products table where name = name
 void db_delete_products(sqlite3 *db, char *name) {
-    int rc = sqlite3_open("database.db", &db);
+    int rc = open_database(db);
 
-    char sql[128];
+    char sql[64];
     char *err_msg;
 
     sprintf(sql, "delete from products where name = '%s';", name);
@@ -56,11 +70,49 @@ void db_delete_products(sqlite3 *db, char *name) {
     }
 
     sqlite3_close(db);
-
 }
 
+// return string containing all columns of record from products table
+// where name = name. returns null if name not in table
+char *db_select_products(sqlite3 *db, char *name) {
+    int rc = open_database(db);
+    sqlite3_stmt* stmt = 0;
+    char *result = NULL;
 
+    char sql[128];
+    sprintf(sql,
+            "select name, category, price, stock \
+            from products where name = '%s'", name);
 
+    // convert sql text to prepared statement object and returns a pointer
+    // to it. the sqlite3_step runs the object code to get result
+    // full explanation at https://sqlite.org/cintro.html
+    rc = sqlite3_prepare_v2(db, sql, strlen(sql), &stmt, NULL);
 
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+    } else {
+        while (sqlite3_step(stmt) == SQLITE_ROW) {
+            // get size of result str
+            ssize_t needed = snprintf(
+                NULL, 0, "%s %s %s %s",
+                sqlite3_column_text(stmt, 0),
+                sqlite3_column_text(stmt, 1),
+                sqlite3_column_text(stmt, 2),
+                sqlite3_column_text(stmt, 3)
+            );
 
+            result = malloc(needed + 1);
+            sprintf(result,
+                "%s %s %s %s",
+                sqlite3_column_text(stmt, 0),
+                sqlite3_column_text(stmt, 1),
+                sqlite3_column_text(stmt, 2),
+                sqlite3_column_text(stmt, 3));
+        }
+    }
+
+    sqlite3_close(db);
+    return result;
+}
 
